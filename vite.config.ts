6a4295a8cf8abc3014680ca3c99e16a2f21cb24b
zyronon <zyronon@163.com>
2024-05-01 23:26:06 +0800

    save123

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {getLastCommit} from "git-last-commit";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
+  const latestCommitHash = await new Promise<string>((resolve) => {
+    return getLastCommit((err, commit) => (err ? 'unknown' : resolve(commit.shortHash)))
+  })
   return {
+    base: './',
+    envDir: 'env',
     plugins: [vue()],
     esbuild: {
       // drop: ['console', 'debugger']
