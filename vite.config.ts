96381a67debe160cfd006b4fee6aa720d7cbb301
zyronon <zyronon@163.com>
2024-05-01 23:14:40 +0800

    save

--- vite.config.ts
+++ vite.config.ts
@@ -25,95 +25,98 @@ export default defineConfig(async () => {
   return {
     base: './',
     // envDir: 'env',
-    plugins: [
-      VueMacros({
-        plugins: {
-          vue: Vue(),
-          vueJsx: VueJsx() // if needed
-        }
-        // betterDefine: true,
-        // reactivityTransform: {
-        //   exclude: [/node_modules/, /jQuery\.js/]
-        // }
-      }),
-      // Vue(),
-      // VueJsx(),
-      lifecycle === 'report' ? (visualizer({ open: false }) as any as PluginOption) : null,
-      importToCDN({
-        modules: [
-          {
-            name: 'vue',
-            var: 'Vue',
-            path: `https://lib.baomitu.com/vue/3.4.21/vue.runtime.global.prod.min.js`
-          },
-          {
-            name: 'vue-router',
-            var: 'VueRouter',
-            path: 'https://lib.baomitu.com/vue-router/4.3.0/vue-router.global.prod.min.js'
-          },
-          {
-            name: 'vue-demi',
-            var: 'VueDemi',
-            path: 'https://lib.baomitu.com/vue-demi/0.14.7/index.iife.min.js'
-          },
-          {
-            name: 'mockjs',
-            var: 'Mock',
-            path: 'https://lib.baomitu.com/Mock.js/1.0.1-beta3/mock-min.js'
-          }
-        ]
-      })
-      // viteCompression({
-      //   verbose: false,
-      //   disable: false,
-      //   threshold: 10240,
-      //   algorithm: 'brotliCompress',
-      // }),
-      // viteCompression({
-      //   verbose: false,
-      //   disable: false,
-      //   algorithm: 'gzip',
-      //   threshold: 10240,
-      // }),
-      // viteImagemin({
-      //   gifsicle: {
-      //     optimizationLevel: 7,
-      //     interlaced: false,
-      //   },
-      //   optipng: {
-      //     optimizationLevel: 7,
-      //   },
-      //   mozjpeg: {
-      //     quality: 20,
-      //   },
-      //   pngquant: {
-      //     quality: [0.8, 0.9],
-      //     speed: 4,
-      //   },
-      //   svgo: {
-      //     plugins: [
-      //       {
-      //         name: 'removeViewBox',
-      //       },
-      //       {
-      //         name: 'removeEmptyAttrs',
-      //         active: false,
-      //       },
-      //     ],
-      //   },
-      // }),
+    plugins:[
+      Vue(),
     ],
-    define: {
-      LATEST_COMMIT_HASH: JSON.stringify(
-        latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')
-      )
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-    },
+    // plugins: [
+    //   VueMacros({
+    //     plugins: {
+    //       vue: Vue(),
+    //       vueJsx: VueJsx() // if needed
+    //     }
+    //     // betterDefine: true,
+    //     // reactivityTransform: {
+    //     //   exclude: [/node_modules/, /jQuery\.js/]
+    //     // }
+    //   }),
+    //   // Vue(),
+    //   // VueJsx(),
+    //   lifecycle === 'report' ? (visualizer({ open: false }) as any as PluginOption) : null,
+    //   importToCDN({
+    //     modules: [
+    //       {
+    //         name: 'vue',
+    //         var: 'Vue',
+    //         path: `https://lib.baomitu.com/vue/3.4.21/vue.runtime.global.prod.min.js`
+    //       },
+    //       {
+    //         name: 'vue-router',
+    //         var: 'VueRouter',
+    //         path: 'https://lib.baomitu.com/vue-router/4.3.0/vue-router.global.prod.min.js'
+    //       },
+    //       {
+    //         name: 'vue-demi',
+    //         var: 'VueDemi',
+    //         path: 'https://lib.baomitu.com/vue-demi/0.14.7/index.iife.min.js'
+    //       },
+    //       {
+    //         name: 'mockjs',
+    //         var: 'Mock',
+    //         path: 'https://lib.baomitu.com/Mock.js/1.0.1-beta3/mock-min.js'
+    //       }
+    //     ]
+    //   })
+    //   // viteCompression({
+    //   //   verbose: false,
+    //   //   disable: false,
+    //   //   threshold: 10240,
+    //   //   algorithm: 'brotliCompress',
+    //   // }),
+    //   // viteCompression({
+    //   //   verbose: false,
+    //   //   disable: false,
+    //   //   algorithm: 'gzip',
+    //   //   threshold: 10240,
+    //   // }),
+    //   // viteImagemin({
+    //   //   gifsicle: {
+    //   //     optimizationLevel: 7,
+    //   //     interlaced: false,
+    //   //   },
+    //   //   optipng: {
+    //   //     optimizationLevel: 7,
+    //   //   },
+    //   //   mozjpeg: {
+    //   //     quality: 20,
+    //   //   },
+    //   //   pngquant: {
+    //   //     quality: [0.8, 0.9],
+    //   //     speed: 4,
+    //   //   },
+    //   //   svgo: {
+    //   //     plugins: [
+    //   //       {
+    //   //         name: 'removeViewBox',
+    //   //       },
+    //   //       {
+    //   //         name: 'removeEmptyAttrs',
+    //   //         active: false,
+    //   //       },
+    //   //     ],
+    //   //   },
+    //   // }),
+    // ],
+    // define: {
+    //   LATEST_COMMIT_HASH: JSON.stringify(
+    //     latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')
+    //   )
+    // },
+    // resolve: {
+    //   alias: {
+    //     '@': fileURLToPath(new URL('./src', import.meta.url))
+    //   },
+    //   extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    // },
     build: {
       sourcemap: false,
       rollupOptions: {
