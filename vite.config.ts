7cd72f49f738dd58a1c33170282bbfff99512753
zyronon <zyronon@163.com>
2024-05-02 01:17:48 +0800

    12

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,22 @@ import Vue from '@vitejs/plugin-vue'
 import {getLastCommit} from 'git-last-commit'
 
 export default defineConfig(() => {
-  return new Promise(async resolve => {
-    const latestCommitHash = await new Promise<string>((resolve) => {
-      return getLastCommit((err, commit) => (err ? 'unknown' : resolve(commit.shortHash)))
-    })
-    resolve({
-      plugins: [
-        Vue()
-      ],
-      define: {
-        LATEST_COMMIT_HASH: JSON.stringify(
-          latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')
-        )
-      },
+  return new Promise(resolve => {
+    let latestCommitHash = ''
+    getLastCommit((err, commit) => {
+      if (!err) {
+        latestCommitHash = commit.shortHash
+      }
+      resolve({
+        plugins: [
+          Vue()
+        ],
+        define: {
+          LATEST_COMMIT_HASH: JSON.stringify(
+            latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')
+          )
+        },
+      })
     })
   })
 })
