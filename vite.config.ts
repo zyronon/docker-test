bfe8e2d65add8a5283f14c87b6eefd4f8aaa9606
zyronon <zyronon@163.com>
2024-05-01 23:24:46 +0800

    save12

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  esbuild: {
-    // drop: ['console', 'debugger']
-  },
-  server: {
-    port: 3000,
-    open: true,
-    host: '0.0.0.0',
-    fs: {
-      strict: false
+export default defineConfig(async () => {
+  return {
+    plugins: [vue()],
+    esbuild: {
+      // drop: ['console', 'debugger']
+    },
+    server: {
+      port: 3000,
+      open: true,
+      host: '0.0.0.0',
+      fs: {
+        strict: false
+      }
     }
   }
 })
